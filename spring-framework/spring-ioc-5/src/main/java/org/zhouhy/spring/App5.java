a2b246d8a9c1b85323c1553c9b220100e2f060d5
fengandzhy <fengandzhy@gmail.com>
2021-04-20 05:28:22 +1200

    spring-ioc-5

---
+++ spring-framework/spring-ioc-5/src/main/java/org/zhouhy/spring/App5.java
@@ -0,0 +1,12 @@
+package org.zhouhy.spring;
+
+
+/**
+ * 
+ * 
+ * */
+public class App5 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ spring-framework/spring-ioc-5/src/main/java/org/zhouhy/spring/factorybean/ConnectionFactoryBean.java
@@ -0,0 +1,38 @@
+package org.zhouhy.spring.factorybean;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.zhouhy.spring.entity.DBParameters;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactoryBean implements FactoryBean<Connection> {
+
+    private DBParameters dbParameters;    
+    
+    @Override
+    public Connection getObject() throws Exception {
+        Class.forName(dbParameters.getDriver());
+        Connection connection = 
+                DriverManager.getConnection(dbParameters.getUrl(),dbParameters.getUsername(),dbParameters.getPassword());
+        return connection;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Connection.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public DBParameters getDbParameters() {
+        return dbParameters;
+    }
+
+    public void setDbParameters(DBParameters dbParameters) {
+        this.dbParameters = dbParameters;
+    }
+}
