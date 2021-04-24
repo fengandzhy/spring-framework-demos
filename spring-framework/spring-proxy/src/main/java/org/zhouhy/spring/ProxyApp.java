cbc97737c707ec69e936105fad952e44c353481f
fengandzhy <fengandzhy@gmail.com>
2021-04-24 21:40:10 +1200

    spring-proxy some comments

--- spring-framework/spring-proxy/src/main/java/org/zhouhy/spring/ProxyApp.java
+++ spring-framework/spring-proxy/src/main/java/org/zhouhy/spring/ProxyApp.java
@@ -5,6 +5,10 @@ package org.zhouhy.spring;
  * 
  * 2. cglib是针对类来实现代理的，他的原理是对指定的目标类生成一个子类，并覆盖其中方法实现增强，但因为采用的是继承，所以不能对final修饰的类进行代理
  * 
+ * 3. JDK动态代理是实现 InvocationHandler接口, 使用invoke()方法进行面向切面的处理 
+ * 
+ * 4. cglib动态代理是实现MethodInterceptor接口, 使用intercept()方法进行面向切面的处理 
+ * 
  * 
  * */
 public class ProxyApp {
