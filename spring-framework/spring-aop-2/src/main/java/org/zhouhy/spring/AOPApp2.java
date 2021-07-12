339082e0db24559107d9138d4a69898d6192abba
fengandzhy <fengandzhy@gmail.com>
2021-07-12 20:24:28 +1200

    spring-aop2

--- spring-framework/spring-aop-2/src/main/java/org/zhouhy/spring/AOPApp2.java
+++ spring-framework/spring-aop-2/src/main/java/org/zhouhy/spring/AOPApp2.java
@@ -1,6 +1,11 @@
 package org.zhouhy.spring;
 
 /**
+ * 1. 用advisor 的它的这个aspect类是有要求的, 例如这个DeveloperHelper 它必须要实现MethodBeforeAdvice,AfterReturningAdvice
+ * 才能实现方法前增强和返回后增强
+ * 
+ * 2. 用aop:aspect的, 它的这个aspect类是没有多大要求但是在配置的时候, 具体配置哪种增强是要在配置文件中体现的.
+ * 
  * 
  * */
 public class AOPApp2 {
