b2a00d655a2cf5d2a3abbbb871a99ac8738be984
fengandzhy <fengandzhy@gmail.com>
2021-07-12 20:33:07 +1200

    AOPApp3

---
+++ spring-framework/spring-aop-3/src/main/java/org/zhouhy/spring/AOPApp3.java
@@ -0,0 +1,16 @@
+package org.zhouhy.spring;
+
+/**
+ * 1. 用配置文件来实现AOP, 主要有以下几个步骤
+ *  1) 配置目标类calculator
+ *  2) 配置aspect类 loggerAspect
+ *  3) 配置pointcut 就是那个含有表达式的部分
+ *  4) 在aop:aspect配置前后增强
+ * 
+ * 
+ * */
+public class AOPApp3 {
+    public static void main(String[] args) {
+        
+    }
+}
