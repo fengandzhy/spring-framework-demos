2512c00b61c5befa1e47402066bf0604409c8277
fengandzhy <fengandzhy@gmail.com>
2024-02-03 10:43:23 +1300

    spring-ioc aop-1

--- spring/spring-aop-1/src/main/java/org/frank/spring/aop/aspect/LoggingAspect.java
+++ spring/spring-aop-1/src/main/java/org/frank/spring/aop/aspect/LoggingAspect.java
@@ -1,4 +1,30 @@
 package org.frank.spring.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+// 使用@Aspect注解标注这是一个切面
+@Aspect
+@Component
 public class LoggingAspect {
+    
+    // 定义一个切点，匹配UserService中的所有方法
+    @Pointcut("execution(* org.frank.spring.aop.target.UserService.*(..))")
+    public void userServiceMethods() {}
+
+    // 在方法执行之前的通知
+    @Before("userServiceMethods()")
+    public void beforeAddingUser(JoinPoint joinPoint) {
+        System.out.println("Before method: " + joinPoint.getSignature().getName());
+    }
+
+    // 在方法执行之后的通知
+    @After("userServiceMethods()")
+    public void afterAddingUser(JoinPoint joinPoint) {
+        System.out.println("After method: " + joinPoint.getSignature().getName());
+    }
 }
